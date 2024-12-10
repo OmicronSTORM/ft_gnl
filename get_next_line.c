b4cf90e1e18958b6fc2f0ee3736d235c6064172e
Justin Owoundi Nlate <jowoundi@fu-r7-p3.s19.be>
2024-12-10 18:40:55 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jowoundi <jowoundi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 18:46:06 by jowoundi          #+#    #+#             */
-/*   Updated: 2024/12/05 17:08:22 by jowoundi         ###   ########.fr       */
+/*   Updated: 2024/12/10 18:03:29 by jowoundi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,89 @@
 
 void	readbuff(int fd, char *line)
 {
-	int		caract;
+	int		i;
+	int		j;
 	char	*temp;
 
-	caract = 0;
-	temp = NULL;
+	i = 0;
+	j = 0;
+	temp = "";
 	while (!ft_strchr(line, '\n'))
 	{
 		temp = ft_strjoin(temp, line);
 		read(fd, line, BUFFER_SIZE);
-		temp = line;
 	}
-	printf("%s", temp);
+	j = ft_strlen(temp);
+	while (line[i] != '\n')
+		{
+			temp[j++] = line[i++];
+		}
+	temp[j] = '\0';
+	printf("%s\n", temp);
+}
+
+void	nextline(char *tmp_buff)
+{
+	int	i;
+	int	j;
+	int	len_tmp;
+	char	*str;
+	char	*tmp;
+	
+	str = "";
+	i = 0;
+	j = 0;
+	if (!tmp_buff)
+		return ;
+	while (tmp_buff[i])
+		i++;
+	printf("%d\n", i);
+	i = 0;
+	str = tmp_buff;
+	printf("%s\n", str);
+	while (str[i] && str[i] != '\n')
+		i++;
+	while (str[i] && str[i] == '\n')
+		i++;
+	len_tmp = i;
+	while (str[len_tmp])
+	{
+		j++;
+		len_tmp++;
+	}
+	printf("%d\n", j);
+	printf("%d\n", i);
+	tmp = malloc(sizeof(char) * j);
+	j = 0;
+	// printf("%s\n", tmp);
+	while(str[i])
+	{
+		tmp[j] = str[i];
+		i++;
+		j++;
+	}
+	printf("%s\n", tmp);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*line;
+	char	*line;
 	static char	tmp_buff[BUFFER_SIZE];
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	readbuff(fd, line);
-	return (0);
+	readbuff(fd, tmp_buff);
+	if (!tmp_buff || tmp_buff[0] == '\0')
+		return (NULL);
+	nextline(tmp_buff);
+	return (NULL);
 }
 int main()
 {
 	int fd;
+	int i;
 	
+	i = 0;
 	fd = open("test.txt", O_RDONLY);
 	get_next_line(fd);
 	return (0);
