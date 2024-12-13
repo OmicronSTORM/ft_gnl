e05f1ae3389d1d603b3c4f5f980ed7f5a26a7678
Justin Owoundi Nlate <jowoundi@fu-r8-p3.s19.be>
2024-12-13 19:09:35 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: jowoundi <jowoundi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 18:46:06 by jowoundi          #+#    #+#             */
-/*   Updated: 2024/12/11 18:47:38 by jowoundi         ###   ########.fr       */
+/*   Updated: 2024/12/13 19:09:07 by jowoundi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	readbuff(int fd, char *line)
+char	*readbuff(int fd, char *tmp_buff)
 {
 	int		i;
 	int		j;
@@ -20,96 +20,77 @@ void	readbuff(int fd, char *line)
 
 	i = 0;
 	j = 0;
-	temp = line;
+	temp = tmp_buff;
 	// printf("%s\n", temp);
-	while (!ft_strchr(line, '\n'))
+	while (!ft_strchr(tmp_buff, '\n'))
 	{
-		temp = ft_strjoin(temp, line);
-		read(fd, line, BUFFER_SIZE);
+		temp = ft_strjoin(temp, tmp_buff);
+		read(fd, tmp_buff, BUFFER_SIZE);
 	}
 	j = ft_strlen(temp);
-	while (line[i] != '\n')
+	while (tmp_buff[i] != '\n')
 		{
-			temp[j++] = line[i++];
+			temp[j++] = tmp_buff[i++];
 		}
 	temp[j] = '\0';
 	// printf("%s\n", temp);
+	return (temp);
 }
-// void	start_next_line(char *tmp_buff)
-// {
-// 	int	i;
-// 	int	j;
-// 	int	len_tmp;
-// 	char	*str;
-// 	char	*tmp;
-	
-// }
-
-void	next_line_bis(char **tmp_buff, char *str, int len_tmp, int i)
-{
-	int	j;
-	char	*tmp;
-	
-	j = 0;
-	while (str[len_tmp])
-	{
-		len_tmp++;
-		j++;
-	}
-	tmp = malloc(sizeof(char) * j);
-	if (!tmp)
-		return ;
-	j = 0;
-	while (str[i])
-		tmp[j++] = str[i++];
-	*tmp_buff = tmp;
-}
-
-void	nextline(char *tmp_buff)
+char	*re_rest(char *tmp_buff)
 {
 	int	i;
 	int	j;
-	int	len_tmp;
-	char	*str;
-	
-	str = "";
+	int	len_temp;
+	char	*rest;
+
 	i = 0;
 	j = 0;
-	if (!tmp_buff)
-		return ;
+	while (tmp_buff[i] && tmp_buff[i] != '\n')
+		i++;
+	if (tmp_buff[i] && tmp_buff[i] == '\n')
+		i++;
+	len_temp = i;
+	while (tmp_buff[len_temp])
+	{
+		len_temp++;
+		j++;
+	}
+	rest = malloc(sizeof(char) * j);
+	j = 0;
 	while (tmp_buff[i])
-		i++;
-	i = 0;
-	str = tmp_buff;
-	while (str[i] && str[i] != '\n')
-		i++;
-	while (str[i] && str[i] == '\n')
-		i++;
-	len_tmp = i;
-	next_line_bis(&tmp_buff, str, len_tmp, i);
-	printf("%s\n", tmp_buff);
+		rest[j++] = tmp_buff[i++];
+	printf("rest:\n%s\n", rest);
+	return (rest);
+	// dans cette fonction je vais ajuster le reste pour qu'il soit correct pour le prochain appel de gnl
 }
 
 char	*get_next_line(int fd)
 {
 	char	*line;
+	static char	*rest;
 	static char	tmp_buff[BUFFER_SIZE];
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	readbuff(fd, tmp_buff);
+	line = readbuff(fd, tmp_buff);
+	printf("line:\n%s\n", line);
+	printf("tmp_buff:\n%s\n", tmp_buff);
 	if (!tmp_buff || tmp_buff[0] == '\0')
 		return (NULL);
-	nextline(tmp_buff);
-	return (NULL);
+	rest = re_rest(tmp_buff);
+	// create_line();
+	// nextline(tmp_buff);
+	return (line);
 }
 int main()
 {
 	int fd;
 	int i;
-	
+	char *line;
+
 	i = 0;
 	fd = open("test.txt", O_RDONLY);
-	get_next_line(fd);
+	line = get_next_line(fd);
+	// printf("%s\n", line);
 	return (0);
 }
